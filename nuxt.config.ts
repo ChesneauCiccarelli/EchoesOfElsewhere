ca2e235bdfdaaa589ae2c8372070027d9cab3352
Lilian <chci.lilian@gmail.com>
2024-01-29 15:50:21 +0100

    setup : Variables / Unstyling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxtjs/google-fonts'],
   components: [
     {
       path: '~/components',
@@ -18,6 +18,12 @@ export default defineNuxtConfig({
         @import "@/scss/foundations/_mixins.scss";`,
         },
       },
+    },
+  },
+  googleFonts: {
+    families: {
+      'Golos+Text': true,
+      'Major+Mono+Display': true,
     },
   },
 });
